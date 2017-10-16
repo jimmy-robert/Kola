dd5003389a26ced36cad5b2d8e26800c08b95624
Jimmy Robz <jimmy@robz.re>
2017-10-16 09:20:02 +0200

    Fix recycler module after updating ui module to use AnkoComponent.

--- kola-recycler/src/main/java/io/kola/recycler/RecyclerItems.kt
+++ kola-recycler/src/main/java/io/kola/recycler/RecyclerItems.kt
@@ -1,6 +1,7 @@
 package io.kola.recycler
 
 import android.view.ViewGroup
+import io.kola.ui.createView
 
 infix fun <T: RecyclerItem> T.from(parent: ViewGroup) = apply {
     createView(parent.context)
