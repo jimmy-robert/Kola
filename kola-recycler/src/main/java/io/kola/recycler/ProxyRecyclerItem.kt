485ce5a549954da4d905970ede59f054e1cf984c
Jimmy Robz <jimmy@robz.re>
2017-10-28 16:23:41 +0200

    Improve ProxyRecyclerItem withBase function.

--- kola-recycler/src/main/java/io/kola/recycler/ProxyRecyclerItem.kt
+++ kola-recycler/src/main/java/io/kola/recycler/ProxyRecyclerItem.kt
@@ -4,7 +4,5 @@ import io.kola.ui.UiComponent
 
 class ProxyRecyclerItem<out T: UiComponent>(private val base: T): RecyclerItem() {
     override fun createView() = base.createView(context)
-    fun withBase(block: T.() -> Unit) {
-        base.apply(block)
-    }
+    fun withBase(block: T.() -> Unit) = block(base)
 }
