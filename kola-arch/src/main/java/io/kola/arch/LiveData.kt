144a703e657081aed7cd28d7723d6b7cca15e78c
Jimmy Robz <jimmy@robz.re>
2017-11-16 09:21:07 +0100

    Create simple function to create a mutable live data.

--- kola-arch/src/main/java/io/kola/arch/LiveData.kt
+++ kola-arch/src/main/java/io/kola/arch/LiveData.kt
@@ -2,8 +2,11 @@ package io.kola.arch
 
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 
 fun <T> LiveData<T>.observe(lifecycleOwner: LifecycleOwner, callback: (T?) -> Unit) {
     observe(lifecycleOwner, Observer<T> { callback(it) })
 }
+
+fun <T> liveData() = MutableLiveData<T>()
