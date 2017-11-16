96938caaa9fb62602d6e441a8b33dd91860ecce8
Jimmy Robz <jimmy@robz.re>
2017-11-16 01:26:36 +0100

    Remove useless extension and force recycler item parent in constructor.

--- kola-recycler/src/main/java/io/kola/recycler/RecyclerItem.kt
+++ kola-recycler/src/main/java/io/kola/recycler/RecyclerItem.kt
@@ -1,10 +1,25 @@
 package io.kola.recycler
 
+import android.view.ViewGroup
 import io.kola.ui.UiComponent
+import io.kola.ui.createView
 
-abstract class RecyclerItem: UiComponent() {
+abstract class RecyclerItem(parent: ViewGroup) : UiComponent() {
+
+    init {
+        createView(parent.context)
+    }
 
     lateinit var viewHolder: ItemViewHolder private set
+
+    val adapterPosition get() = viewHolder.adapterPosition
+    var isRecyclable
+        get() = viewHolder.isRecyclable
+        set(value) = viewHolder.setIsRecyclable(value)
+    val itemId get() = viewHolder.itemId
+    val itemViewType get() = viewHolder.itemViewType
+    val layoutPosition get() = viewHolder.layoutPosition
+    val oldPosition get() = viewHolder.oldPosition
 
     override fun onViewCreated() {
         super.onViewCreated()
--- kola-recycler/src/main/java/io/kola/recycler/RecyclerItems.kt
+++
@@ -1,8 +0,0 @@
-package io.kola.recycler
-
-import android.view.ViewGroup
-import io.kola.ui.createView
-
-infix fun <T: RecyclerItem> T.from(parent: ViewGroup) = apply {
-    createView(parent.context)
-}
