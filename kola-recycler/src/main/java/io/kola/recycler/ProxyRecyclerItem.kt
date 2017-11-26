8e1206108b754aa469831d69a35e6ce9538f396e
Jimmy Robz <jimmy@robz.re>
2017-11-26 17:13:37 +0400

    Fix missing constructor parameter.

--- kola-recycler/src/main/java/io/kola/recycler/ProxyRecyclerItem.kt
+++ kola-recycler/src/main/java/io/kola/recycler/ProxyRecyclerItem.kt
@@ -1,8 +1,9 @@
 package io.kola.recycler
 
+import android.view.ViewGroup
 import io.kola.ui.UiComponent
 
-class ProxyRecyclerItem<out T: UiComponent>(private val base: T): RecyclerItem() {
+class ProxyRecyclerItem<out T : UiComponent>(parent: ViewGroup, private val base: T) : RecyclerItem(parent) {
     override fun createView() = base.createView(context)
     fun withBase(block: T.() -> Unit) = block(base)
 }
