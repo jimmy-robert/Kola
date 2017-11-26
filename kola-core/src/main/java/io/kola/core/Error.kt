cf5edd1b310c6fb20981107549b861f5ded935d4
Jimmy Robz <jimmy@robz.re>
2017-11-26 17:12:00 +0400

    Enhance generic errors.

--- kola-core/src/main/java/io/kola/core/Error.kt
+++ kola-core/src/main/java/io/kola/core/Error.kt
@@ -1,3 +1,5 @@
 package io.kola.core
 
-fun noGetter(): Nothing = throw IllegalAccessException("No getter for this property")
+fun noGetter(propertyName: String = "this property"): Nothing = throw IllegalAccessException("No getter for $propertyName")
+
+fun unreachable(): Nothing = throw IllegalAccessException("Unreachable code")
